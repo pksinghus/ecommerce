d575d26c0ed877e51c6998807c0fd25f1e728368
EC2 Default User <ec2-user@ip-172-31-37-15.us-east-2.compute.internal>
2020-07-07 16:52:03 +0000

    Templates Header, Footer, and DB Items

--- src/main/resources/db/migration/V2__import_default_catalog.sql
+++ src/main/resources/db/migration/V2__import_default_catalog.sql
@@ -1,6 +1,6 @@
-insert into catalog values ('abcr', 'All Butter Croissant', 0.75);
-insert into catalog values ('ccr', 'Chocolate Croissant', 0.95);
-insert into catalog values ('b', 'Fresh Baguette', 1.60);
-insert into catalog values ('rv', 'Red Velvet', 3.95);
-insert into catalog values ('vs', 'Victoria Sponge', 5.45);
-insert into catalog values ('cc', 'Carrot Cake', 3.45);
+insert into catalog values ('abcr', 'Face Mask', 04.99);
+insert into catalog values ('ccr', 'Hand Wash', 04.99);
+insert into catalog values ('b', 'Clorox Wipes', 04.99);
+insert into catalog values ('rv', 'Hand Sanitizer', 03.99);
+insert into catalog values ('vs', 'Cleaning Spray', 03.99);
+insert into catalog values ('cc', 'Gloves', 14.99);
